7e6f01308adefe85755bff38039fc5258241e1f8
Rahul Mishra <rahul.mishra@screen-magic.com>
2016-07-13 21:16:26 +0530

    committing frinted chages

---
+++ frontend/src/mainApp.js
@@ -0,0 +1,15 @@
+var mainApp = angular.module("mainApp", ['ngRoute', 'ngStorage']);
+mainApp.config(function($routeProvider) {
+    $routeProvider.
+    when('/auth', {
+        templateUrl: 'src/login/login.html',
+        controller: 'loginController'
+    }).
+    when('/signup', {
+        templateUrl: 'src/signup/signup.html'
+        // controller: 'loginController'
+    }).
+    otherwise({
+        redirectTo: '/'
+    });
+});
