595b7f4bb33f3f45f2e6e907b038714d1abb4d7e
Rahul91 <priyrahulmishra@gmail.com>
2016-07-15 10:05:00 +0530

    fronted changes

--- frontend/src/mainApp.js
+++ frontend/src/mainApp.js
@@ -12,10 +12,25 @@ mainApp.config(function($routeProvider) {
     when('/home', {
         templateUrl: 'src/auth/auth.html'
     }).
-    // when('/login', {
-    //     // templateUrl: 'src/auth/auth.html#login'
-    // }).
+    when('/', {
+        controller: 'indexController'
+    }).
     otherwise({
         redirectTo: '/'
     });
+});
+
+
+mainApp.controller("indexController", function () {
+    $scope.closeNav = function () {
+        document.getElementById("mySidenav").style.width = "0";
+        document.getElementById("main").style.marginLeft= "0";
+        document.body.style.backgroundColor = "white";
+    };
+    $scope.openNav = function () {
+        document.getElementById("mySidenav").style.width = "250px";
+        document.getElementById("main").style.marginLeft = "250px";
+        document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+    };
+
 });
