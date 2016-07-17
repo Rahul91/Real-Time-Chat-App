e55b8cd1beed9f1a710a579784f4ec2e7e0a57ab
Rahul Mishra <rahul.mishra@screen-magic.com>
2016-07-18 01:40:39 +0530

    Added channel tables, deleting images, landing page updated

---
+++ backend/scripts/migrate_db.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+print_help() {
+  echo "Usage:
+  ./migrate_db.sh
+  Options:
+    upgrade <no.>                       Ex: upgrade 1 will upgrade 1 revision
+    downgrade <no.>                     Ex: downgrade 2 will downgrade 2 next revision
+  "
+}
+
+print_help
+
+if [[ $# > 0 ]]; then
+  if [[ "$1" == "upgrade" ]] && [[ ! -z "$2" ]]; then
+    (cd ../healthify/models && alembic -c alembic.ini  upgrade +"$2" )
+  elif [[ "$1" == "downgrade" ]] && [[ ! -z "$2" ]]; then
+    (cd ../healthify/models && alembic -c alembic.ini  downgrade -"$2" )
+  fi
+else
+    (cd ../healthify/models && alembic -c alembic.ini upgrade head )
+  exit 1
+fi
